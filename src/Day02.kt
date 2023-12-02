aa65bd2328126dd63650bf9179de79451ade496e
Christian Birmes <chbirmes@gmail.com>
2023-12-02 22:28:56 +0100

    refactoring

--- src/Day02.kt
+++ src/Day02.kt
@@ -3,7 +3,7 @@ import kotlin.math.max
 fun main() {
 
     val maxCubes = mapOf("red" to 12, "green" to 13, "blue" to 14)
-    val powerColors = maxCubes.keys
+    val colors = maxCubes.keys
 
     data class Game(val id: Int, val cubeSets: List<Map<String, Int>>) {
 
@@ -11,27 +11,32 @@ fun main() {
 
         fun possible() = cubeSets.all { it.possible() }
 
-        fun minimumSet(): Map<String, Int> = cubeSets.fold(emptyMap()) { x, y ->
-            val allColors = x.keys + y.keys
-            allColors.associateWith { max(x[it] ?: 0, y[it] ?: 0) }
-        }
+        fun minimumSet(): Map<String, Int> =
+            cubeSets.fold(emptyMap()) { x, y ->
+                colors.associateWith { max(x[it] ?: 0, y[it] ?: 0) }
+            }
     }
 
-    fun parseGame(line: String) = line.split(':', ';').let { gameTagAndTurns ->
-        val gameId = gameTagAndTurns.first().substringAfter(' ').toInt()
-        val cubeSets = gameTagAndTurns.drop(1).map { turn ->
-            turn.split(',').associate { countAndColor ->
-                countAndColor.trim().split(' ').let {
-                    it[1] to it[0].toInt()
-                }
+    fun parseGame(line: String) =
+        line.split(':', ';')
+            .let { gameTagAndTurns ->
+                val gameId = gameTagAndTurns.first().substringAfter(' ').toInt()
+                val cubeSets = gameTagAndTurns
+                    .drop(1)
+                    .map { turn ->
+                        turn.split(',')
+                            .associate { countAndColor ->
+                                countAndColor.trim()
+                                    .split(' ')
+                                    .let { it[1] to it[0].toInt() }
+                            }
+                    }
+                Game(gameId, cubeSets)
             }
-        }
-        Game(gameId, cubeSets)
-    }
 
     fun part1(input: List<String>) = input.map { parseGame(it) }.filter { it.possible() }.sumOf { it.id }
 
-    fun Map<String, Int>.power() = powerColors.map { get(it) ?: 0 }.fold(1) { x, y -> x * y }
+    fun Map<String, Int>.power() = colors.map { get(it) ?: 0 }.fold(1) { x, y -> x * y }
 
     fun part2(input: List<String>) = input.map { parseGame(it) }.sumOf { it.minimumSet().power() }
 
