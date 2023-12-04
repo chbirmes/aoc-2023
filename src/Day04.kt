5bf4d398be140d5aff82764d9c37cc689566c45e
Christian Birmes <christian.birmes@cologne-intelligence.de>
2023-12-04 15:52:37 +0100

    Day 4 Solution

---
+++ src/Day04.kt
@@ -0,0 +1,57 @@
+import kotlin.math.min
+
+fun main() {
+
+    data class Scratchcard(val winningNumbers: Set<Int>, val myNumbers: Set<Int>) {
+
+        fun pointValue() = matchingNumbersCount().let {
+            if (it == 0) {
+                0
+            } else {
+                1 shl (it - 1)
+            }
+        }
+
+        fun matchingNumbersCount() = winningNumbers.intersect(myNumbers).size
+
+    }
+
+    val whitespaceRegex = "\\s+".toRegex()
+    fun parseInts(s: String) = s.trim().split(whitespaceRegex).map { it.toInt() }.toSet()
+    fun parseScratchcard(line: String) =
+        line.split(':', '|').let { parts ->
+            Scratchcard(parseInts(parts[1]), parseInts(parts[2]))
+        }
+
+    fun part1(input: List<String>) =
+        input.map { parseScratchcard(it) }.sumOf { it.pointValue() }
+
+    fun List<Pair<Scratchcard, Int>>.scoreLine(index: Int): List<Pair<Scratchcard, Int>> {
+        val original = this
+        val matchingNumbersCount = get(index).first.matchingNumbersCount()
+        val copies = get(index).second
+        return buildList {
+            addAll(original.take(index + 1))
+            val fromIndex = min(index + 1, original.size)
+            val toIndex = min(index + 1 + matchingNumbersCount, original.size)
+            val cardsWithExtraCopies = original.subList(fromIndex, toIndex)
+                .map { it.first to it.second + copies }
+            addAll(cardsWithExtraCopies)
+            addAll(original.drop(toIndex))
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        val scratchcardPile = input.map { parseScratchcard(it) to 1 }
+        val finalPile = scratchcardPile.foldIndexed(scratchcardPile) { index, pile, _ -> pile.scoreLine(index) }
+        return finalPile.sumOf { it.second }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    check(part1(readInput("Day04_test")) == 13)
+    check(part2(readInput("Day04_test2")) == 30)
+
+    val input = readInput("Day04")
+    part1(input).println()
+    part2(input).println()
+}
