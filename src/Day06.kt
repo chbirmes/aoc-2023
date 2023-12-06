19470e3aaad91390c7d8295aec3395c81f66ab29
Christian Birmes <christian.birmes@cologne-intelligence.de>
2023-12-06 16:51:14 +0100

    refactoring

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,21 +1,22 @@
+import kotlin.math.ceil
+import kotlin.math.floor
 import kotlin.math.pow
 import kotlin.math.sqrt
 
 fun main() {
 
+    fun LongRange.length() = last - first + 1
+
     data class Race(val time: Long, val record: Long) {
 
         fun recordBeatingButtonTimes(): LongRange? {
             val timeHalves = time.toDouble() / 2
-            val r = timeHalves.pow(2) - record
+            val r = timeHalves.pow(2) - (record + 1)
             return if (r < 0)
                 null
             else {
-                val lower = (timeHalves - sqrt(r)).toLong() + 1
-                val upper = (timeHalves + sqrt(r)).let {
-                    if (it.rem(1) == 0.0) it - 1 else it
-                }
-                    .toLong()
+                val lower = ceil(timeHalves - sqrt(r)).toLong()
+                val upper = floor(timeHalves + sqrt(r)).toLong()
                 lower..upper
             }
         }
@@ -34,8 +35,7 @@ fun main() {
 
     fun part1(input: List<String>): Long {
         val races = parseRaces(input)
-        return races.mapNotNull { it.recordBeatingButtonTimes() }
-            .map { it.last - it.first + 1 }
+        return races.mapNotNull { it.recordBeatingButtonTimes()?.length() }
             .fold(1) { x, y -> x * y }
     }
 
@@ -44,8 +44,7 @@ fun main() {
             .let { Race(it[0], it[1]) }
 
     fun part2(input: List<String>) =
-        parseSingleRace(input).recordBeatingButtonTimes()!!
-            .let { it.last - it.first + 1 }
+        parseSingleRace(input).recordBeatingButtonTimes()!!.length()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
