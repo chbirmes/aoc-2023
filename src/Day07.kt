4434b77b5b6fa80e0d053aaef4173507af9ca83a
Christian Birmes <christian.birmes@cologne-intelligence.de>
2023-12-07 12:10:29 +0100

    refactoring

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,7 +1,7 @@
 fun main() {
 
     val cardLabels = "AKQJT98765432"
-    val cardLabelsWithJokers = "AKQT98765432J"
+    val cardLabelsWithJoker = "AKQT98765432J"
 
     val typePredicates = listOf<(Collection<Int>, Int) -> Boolean>(
         { counts, jokers -> (counts.maxOrNull() ?: 0) + jokers == 5 },
@@ -15,16 +15,16 @@ fun main() {
 
     data class HandWithBid(val hand: String, val bid: Int) {
 
+        val counts = hand.groupingBy { it }.eachCount()
+
         val typeValue: Int by lazy {
             typePredicates.indexOfFirst { predicate ->
-                val counts = hand.groupingBy { it }.eachCount()
                 predicate(counts.values, 0)
             }
         }
 
         val typeValueWithJokers: Int by lazy {
             typePredicates.indexOfFirst { predicate ->
-                val counts = hand.groupingBy { it }.eachCount()
                 predicate(counts.filterKeys { it != 'J' }.values, counts['J'] ?: 0)
             }
         }
@@ -32,14 +32,16 @@ fun main() {
 
     fun buildComparator(labels: String, typeValueFunction: (HandWithBid) -> Int) =
         Comparator.comparingInt(typeValueFunction)
-            .thenComparing { o1, o2 ->
-                val seq1 = o1.hand.asSequence().map { labels.indexOf(it) }
-                val seq2 = o2.hand.asSequence().map { labels.indexOf(it) }
-                seq1.zip(seq2) { c1, c2 -> c1.compareTo(c2) }.first { it != 0 }
-            }
+            .thenComparing(
+                { handWithBid -> handWithBid.hand.asSequence().map { labels.indexOf(it) } },
+                { indexSequence1, indexSequence2 ->
+                    indexSequence1.zip(indexSequence2, Int::compareTo)
+                        .firstOrNull { it != 0 } ?: 0
+                }
+            )
 
     val comparator = buildComparator(cardLabels) { it.typeValue }
-    val comparatorWithJokers = buildComparator(cardLabelsWithJokers) { it.typeValueWithJokers }
+    val comparatorWithJokers = buildComparator(cardLabelsWithJoker) { it.typeValueWithJokers }
 
     fun parseHandWithBid(line: String) =
         line.split(' ', limit = 2)
